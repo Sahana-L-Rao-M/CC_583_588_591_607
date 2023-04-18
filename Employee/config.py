33a9c89ce92cc9ba319e4396a09627c2512d0455
Sahana Rao <95758122+Sahana-L-Rao-M@users.noreply.github.com>
2023-04-18 12:37:26 +0530

    Add files via upload

---
+++ Employee/config.py
@@ -0,0 +1,6 @@
+customhost = "database-1.ckqwsilv80o5.us-east-2.rds.amazonaws.com"
+customuser = "admin"
+custompass = "niharika123"
+customdb = "employee"
+custombucket = "addgallery1"
+customregion = "us-east-2"
